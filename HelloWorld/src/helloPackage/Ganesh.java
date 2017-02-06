5cdcdf7ad070990129d98a7cf4adc9c9609e7116
ganeshshinde1986 <ganeshshinde1986@gmail.com>
2017-02-06 12:11:26 +0530

    Added a Class implementation.

---
+++ HelloWorld/src/helloPackage/Ganesh.java
@@ -0,0 +1,29 @@
+package helloPackage;
+
+public class Ganesh {
+private int id;
+private String name;
+private String education;
+
+
+public Ganesh()
+{ }
+
+public int getId()
+{
+	id = 1;
+	return id;
+}
+
+public String getName()
+{
+	name = "Ganesh";
+	return name;
+}
+
+public String getEducation()
+{
+	education = "BDAP";
+	return education;
+}
+}
